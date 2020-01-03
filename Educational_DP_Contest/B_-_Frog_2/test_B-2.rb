8e53d14900cabe3b84390c1290148aff3cc8789f
castor4bit <castor.4bit@gmail.com>
2020-01-04 03:34:29 +0900

    DP B - Frog 2 (Ruby) TLE

---
+++ Educational_DP_Contest/B_-_Frog_2/test_B-2.rb
@@ -0,0 +1,47 @@
+require 'test/unit'
+require 'stringio'
+
+class AtCoderSolutionTest < Test::Unit::TestCase
+  sub_test_case "DP_B-2" do
+    def setup
+      $stdout = StringIO.new
+    end
+
+    def teardown
+      $stdin = STDIN
+      $stdout = STDOUT
+    end
+
+    def test_data1
+      data = File.read("data1.txt")
+      $stdin = StringIO.new(data)
+
+      load './B-2.rb'
+      assert_equal "30\n", $stdout.string
+    end
+
+    def test_data2
+      data = File.read("data2.txt")
+      $stdin = StringIO.new(data)
+
+      load './B-2.rb'
+      assert_equal "20\n", $stdout.string
+    end
+
+    def test_data3
+      data = File.read("data3.txt")
+      $stdin = StringIO.new(data)
+
+      load './B-2.rb'
+      assert_equal "0\n", $stdout.string
+    end
+
+    def test_data4
+      data = File.read("data4.txt")
+      $stdin = StringIO.new(data)
+
+      load './B-2.rb'
+      assert_equal "40\n", $stdout.string
+    end
+  end
+end
