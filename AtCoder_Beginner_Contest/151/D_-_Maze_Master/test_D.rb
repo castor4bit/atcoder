195a6c66bbfcf2610c61473d55f492b8797b2ada
castor4bit <castor.4bit@gmail.com>
2020-01-12 22:44:28 +0900

    ABC151 D - Maze Master (Ruby)

---
+++ AtCoder_Beginner_Contest/151/D_-_Maze_Master/test_D.rb
@@ -0,0 +1,31 @@
+require 'test/unit'
+require 'stringio'
+
+class AtCoderSolutionTest < Test::Unit::TestCase
+  sub_test_case "ABC151_D" do
+    def setup
+      $stdout = StringIO.new
+    end
+
+    def teardown
+      $stdin = STDIN
+      $stdout = STDOUT
+    end
+
+    def test_data1
+      data = File.read("data1.txt")
+      $stdin = StringIO.new(data)
+
+      load './D.rb'
+      assert_equal "4\n", $stdout.string
+    end
+
+    def test_data2
+      data = File.read("data2.txt")
+      $stdin = StringIO.new(data)
+
+      load './D.rb'
+      assert_equal "10\n", $stdout.string
+    end
+  end
+end
