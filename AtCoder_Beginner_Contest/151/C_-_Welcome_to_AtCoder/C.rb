e35a1f9880d8694a47199dcd616f6ab2a1836d42
castor4bit <castor.4bit@gmail.com>
2020-01-12 22:53:51 +0900

    Remove unnecessary variables

--- AtCoder_Beginner_Contest/151/C_-_Welcome_to_AtCoder/C.rb
+++ AtCoder_Beginner_Contest/151/C_-_Welcome_to_AtCoder/C.rb
@@ -5,22 +5,18 @@ m.times do
   result.push([p.to_i, s])
 end
 
-penalties = Hash.new(0)
-account = Hash.new(0)
-wacount = Hash.new(0)
+penalties = 0
+ac = Hash.new(0)
+wa = Hash.new(0)
 
 result.each do |r|
   p, s = r
-  if s == "WA"
-    wacount[p] += 1
+  if s == 'AC' and !ac.key?(p)
+    penalties += wa[p]
+    ac[p] = 1
   else
-    penalties[p] = wacount[p] unless penalties.key?(p)
-    account[p] = 1
+    wa[p] += 1
   end
 end
 
-c1 = 0
-c2 = 0
-account.each { |_, v| c1 += v }
-penalties.each { |_, v| c2 += v }
-puts "#{c1} #{c2}"
+puts "#{ac.size} #{penalties}"
