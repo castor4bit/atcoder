2cc80d58c5a25ba0606a71a444af808c7eb07b2c
castor4bit <castor.4bit@gmail.com>
2020-02-02 17:24:52 +0900

    keyence2020 A - Painting (Ruby)

---
+++ keyence/2020/A_-_Painting/A.rb
@@ -0,0 +1,5 @@
+h = gets.to_i
+w = gets.to_i
+n = gets.to_i
+
+puts (1.0 * n / [h, w].max).ceil
---
+++ keyence/2020/A_-_Painting/test_A.rb
@@ -0,0 +1,39 @@
+require 'test/unit'
+require 'stringio'
+
+class AtCoderSolutionTest < Test::Unit::TestCase
+  sub_test_case "keyence2020_A" do
+    def setup
+      $stdout = StringIO.new
+    end
+
+    def teardown
+      $stdin = STDIN
+      $stdout = STDOUT
+    end
+
+    def test_data1
+      data = File.read("data1.txt")
+      $stdin = StringIO.new(data)
+
+      load './A.rb'
+      assert_equal "2\n", $stdout.string
+    end
+
+    def test_data2
+      data = File.read("data2.txt")
+      $stdin = StringIO.new(data)
+
+      load './A.rb'
+      assert_equal "8\n", $stdout.string
+    end
+
+    def test_data3
+      data = File.read("data3.txt")
+      $stdin = StringIO.new(data)
+
+      load './A.rb'
+      assert_equal "2\n", $stdout.string
+    end
+  end
+end
